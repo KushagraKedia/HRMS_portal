e05e0a89f97ab72f87cccf77924de7a77093ed98
Kushagra Kedia <kush123kedia@gmail.com>
2026-07-08 02:27:24 +0530

    Delete login_and_forgot_password directory

--- login_and_forgot_password/src/dashboard/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({children}){
-    return(
-        <button className="bg-white text-black px-8 hover:bg-[#D3D3D3] hover:cursor-pointer rounded flex gap-2">
-            {children}
-        </button>
-    )
-};
-
-export default Button;
